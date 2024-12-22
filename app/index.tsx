fd1b2307e988db09d0861b197a50b10f3601f165
Ammarmyp <ammarmyp@gmail.com>
2024-12-22 11:08:34 +0300

    configure the theme of the app on the expo side

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,41 @@
-import { Text, View } from "react-native";
+import { Text, useColorScheme, View, StyleSheet } from "react-native";
 
 export default function Index() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle =
+    colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-3xl font-extrabold">
+    <View style={[styles.container, themeContainerStyle]}>
+      <Text className="font-extrabold" style={[styles.text, themeTextStyle]}>
         Edit app/index.tsx to edit this screen.
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+  lightContainer: {
+    backgroundColor: "#d0d0c0",
+  },
+  darkContainer: {
+    backgroundColor: "#242c40",
+  },
+
+  lightThemeText: {
+    color: "#242c40",
+  },
+  darkThemeText: {
+    color: "#d0d0c0",
+  },
+});
